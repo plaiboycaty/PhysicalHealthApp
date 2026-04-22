b5b345e759aedf0a9984e4c7903f838ed0b7c1d3
plaiboycaty <mquan8912@gmail.com>
2026-04-23 00:55:16 +0700

    sửa chính tả

--- backend/src/routes/testRoutes.js
+++ backend/src/routes/testRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const testController = require('../controllers/testController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-// Tất cả các hành động liên quan đến Bài Thi y khoa đều là riêng tư -> Cần có Token
+// Tất cả các hành động liên quan đến Bài Test đều là riêng tư -> Cần có Token
 router.get('/:id', authMiddleware, testController.getTestDetail);
 router.post('/submit', authMiddleware, testController.submitTest);
 
